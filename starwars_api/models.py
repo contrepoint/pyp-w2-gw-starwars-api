e7bfdf9c35ac24ab7ec84cd7a4af5f1c3b8cf898
contrepoint <contrepoint21@gmail.com>
2016-10-28 08:49:22 +0000

    implement all BaseModel methods. implement __init__ and count for BaseQuerySet

--- starwars_api/models.py
+++ starwars_api/models.py
@@ -11,7 +11,10 @@ class BaseModel(object):
         Dynamically assign all attributes in `json_data` as instance
         attributes of the Model.
         """
-        pass
+        for key, value in json_data.items():
+            setattr(self, key, value)
+            # setattr(x, 'foobar', 123) is equivalent to x.foobar = 123
+
 
     @classmethod
     def get(cls, resource_id):
@@ -19,7 +22,10 @@ class BaseModel(object):
         Returns an object of current Model requesting data to SWAPI using
         the api_client.
         """
-        pass
+        method_name = getattr(api_client, "get_{}".format(cls.RESOURCE_NAME))
+        # the method_name will equal the method api_client.get_people()
+        json_data = method_name(resource_id) # api_client.get_people(1)
+        return BaseModel(json_data)
 
     @classmethod
     def all(cls):
@@ -28,7 +34,8 @@ class BaseModel(object):
         later in charge of performing requests to SWAPI for each of the
         pages while looping.
         """
-        pass
+        if cls.RESOURCE_NAME == 'people':
+            return PeopleQuerySet()
 
 
 class People(BaseModel):
@@ -55,17 +62,38 @@ class Films(BaseModel):
 class BaseQuerySet(object):
 
     def __init__(self):
-        pass
+        json_data = api_client.get_people()
+        self.total_items = json_data['count']
+        # self.page = 1 # transfer to iter/next?
+        # json_data = api_client.get_people("?page={}").format(self.page)
+        #print(json_data) #  currently printing <starwars_api.models.BaseModel object at 0x7f1d7794c518>
+
 
     def __iter__(self):
-        pass
-
+        #initiate
+        self.page = 1 # transfer to iter/next?
+        self.json_data = api_client.get_people("?page={}").format(self.page)
+         
+        print(json_data)
+        
     def __next__(self):
         """
         Must handle requests to next pages in SWAPI when objects in the current
         page were all consumed.
         """
-        pass
+        # a = People.all() # returns a dict
+        # next_link = a['next'] should return something like http://swapi.co/api/people/?page=2
+        # call this next link after we have exhausted iterating through all People
+        # in a (the initial People.all)
+        # we need to call http://swapi.co/api/people/?page=2
+        # repeat until next is null
+        
+        print(self.json_data)
+        
+        #print(results)
+        # obj = qs.next()
+        # self.assertTrue(isinstance(obj, People))
+        # return a people object People()
 
     next = __next__
 
@@ -75,7 +103,7 @@ class BaseQuerySet(object):
         If the counter is not persisted as a QuerySet instance attr,
         a new request is performed to the API in order to get it.
         """
-        pass
+        return self.total_items
 
 
 class PeopleQuerySet(BaseQuerySet):
